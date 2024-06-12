4590181bf79ea0ca2077a5e6203db9f879a12618
Vantyse <nonsokingsley61@gmail.com>
2024-06-12 17:39:51 +0100

    fix: build errors

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,6 +1,8 @@
+import PageLoader from "@/components/general/PageLoader"
 import BottomMavigation from "@/components/layouts/BottomNavigaiton"
 import Header from "@/components/layouts/Header"
 import Sidebar from "@/components/layouts/Sidebar"
+import { Suspense } from "react"
 
 export default function AppLayout({
   children,
@@ -8,12 +10,14 @@ export default function AppLayout({
   children: React.ReactNode
 }>) {
   return (
-    <div className="grid h-screen overflow-hidden bg-rp-grey-700 lg:grid-cols-[minmax(280px,18%)_1fr]">
-      <Sidebar />
-      <div className="overflow-y-auto p-0 lg:px-4">
-        <div className="">{children}</div>
-        <BottomMavigation />
+    <Suspense fallback={<PageLoader />}>
+      <div className="grid h-screen overflow-hidden bg-rp-grey-700 lg:grid-cols-[minmax(280px,18%)_1fr]">
+        <Sidebar />
+        <div className="overflow-y-auto p-0 lg:px-4">
+          <div className="">{children}</div>
+          <BottomMavigation />
+        </div>
       </div>
-    </div>
+    </Suspense>
   )
 }
